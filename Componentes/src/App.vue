aa18d825072ce600859928984173c6e2cfed706d
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-06-05 17:19:18 -0500

    Componente props

---
+++ Componentes/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <CursoDetalle :nombre="curso.nombre" :precio="curso.precio" :categoria="curso.categoria" :en-inventario="curso.enInventario" ></CursoDetalle>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import CursoDetalle from './components/CursoDetalle.vue';
+
+const curso = ref({
+  nombre: 'Curso de Vue 3',
+  precio: 100,
+  categoria: 'Programación',
+  enInventario: true
+});
+
+
+</script>
---
+++ Componentes/src/components/CursoDetalle.vue
@@ -0,0 +1,25 @@
+<template>
+
+<div class="container">
+        <!--card-->
+        <div class="card mt-5">
+            <div class="card-header">
+                <h1>{{ props.nombre }}</h1>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{ props.precio }}</li>
+                <li class="list-group-item">{{ props.categoria }}</li>
+                <li class="list-group-item"> <span class="badge text-bg-success">{{ props.enInventario? 'En inventario':'Sin inventario' }}</span></li>
+            </ul>
+        </div>
+        <!--fin del card-->
+</div>
+
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps(['nombre', 'precio', 'categoria', 'enInventario']);
+
+
+</script>
