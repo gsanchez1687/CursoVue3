23b5b69be19e99ae4d28996c0ab54d658d85a471
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-08-17 20:26:50 -0500

    ultimos ajustes

--- Componentes/src/main.ts
+++ Componentes/src/main.ts
@@ -1,5 +1,7 @@
 //import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import fontawesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
